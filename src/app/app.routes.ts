ca4e0e1f87944727ac3f0cf130db25938392121d
Dimitar Minchev <dimitar45minchev@gmail.com>
2026-05-26 21:12:41 +0300

    push to work on pc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MinecraftServers } from './minecraft-servers/minecraft-servers';
+import { Home } from './home/home';
+import { Register } from './register/register';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'register', component: Register},
+  { path: 'login', component: Login},
+  { path: 'minecraft-servers', component: MinecraftServers },
+];
---
+++ src/app/minecraft-servers/minecraft-servers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minecraft-servers',
+  imports: [],
+  templateUrl: './minecraft-servers.html',
+  styleUrl: './minecraft-servers.css',
+})
+export class MinecraftServers {}
