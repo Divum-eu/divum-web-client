7f1f4cde3b53940ca81a9b8eab3138e1fceb1383
dimitar45 <dimitar45minchev@gmail.com>
2026-05-31 15:39:18 +0300

    fix: removed unusued imports in @Component

--- src/app/minecraft-servers/minecraft-servers.ts
+++ src/app/minecraft-servers/minecraft-servers.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-minecraft-servers',
-  imports: [],
   templateUrl: './minecraft-servers.html',
   styleUrl: './minecraft-servers.css',
 })
