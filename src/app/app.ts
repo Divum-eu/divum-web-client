11c3e76ff688062a48611c8ac2f48aee85322475
Dimitar Dimitrov <mitkodd2005@gmail.com>
2026-06-26 23:10:40 +0300

    feat: add spartan ui library and needed components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { HlmToasterImports } from '@spartan-ng/helm/sonner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, HlmToasterImports],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
